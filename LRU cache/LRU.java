352d83d167d7d34a4f790db538e036df49df3f56
Sidharth Roy <sidharthroy@Sidharths-MacBook-Air.local>
2025-05-31 15:56:11 +0530

    LRU cache added

---
+++ LRU cache/LRU.java
@@ -0,0 +1,15 @@
+public class LRU{
+    public static void main(String[] args) {
+        LRUCache cache = new LRUCache(2);
+        cache.put(1, 1);
+        cache.put(2, 2);
+        System.out.println(cache.get(1)); 
+        cache.put(3, 3);                 
+        System.out.println(cache.get(2)); 
+        cache.put(4, 4);                 
+        System.out.println(cache.get(1)); 
+        System.out.println(cache.get(3)); 
+        System.out.println(cache.get(4)); 
+        cache.printCache();
+    }
+}
